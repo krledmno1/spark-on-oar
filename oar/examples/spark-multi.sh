6f06eeefe786d0893f08ba45eac5cad5e700010a
Srdjan Krstic <krledmno1@gmail.com>
2016-10-14 11:32:16 +0200

    stopping master fixed

--- oar/examples/spark-multi.sh
+++ oar/examples/spark-multi.sh
@@ -4,9 +4,7 @@
 
 export SPARK_HOME=$HOME/spark  
 export SPARK_JOB_DIR=$HOME/jobs
-
-#????
-export SPARK_PREFIX=$HOME/spark
+export SPARK_PREFIX=$SPARK_HOME
 
 # Create/append spark configuration in SPARK_JOB_DIR from PBS environments. 
 # Specific spark configuration can be put into the SPARK_JOB_DIR/conf/* a priori.
@@ -23,8 +21,7 @@ $SPARK_HOME/sbin/spark-on-hpc.sh start
 #User submissions:
 
 $SPARK_HOME/sbin/spark-submit-on-hpc.sh org.apache.spark.examples.SparkPi $SPARK_HOME/examples/jars/spark-examples_2.11-2.0.1.jar 10
-#add more here...
-
+#change/add more submissions here...
 
 # Stop the spark cluster
 $SPARK_HOME/sbin/spark-on-hpc.sh stop
--- oar/spark-sbin/spark-on-hpc.sh
+++ oar/spark-sbin/spark-on-hpc.sh
@@ -97,7 +97,7 @@ case "$1" in
       done
 
       # stop the master
-      oarsh "SPARK_PREFIX=$SPARK_PREFIX $SPARK_MASTER_IP ${SPARK_HOME}/sbin/spark-daemon-on-hpc.sh --config $SPARK_CONF_DIR stop org.apache.spark.deploy.master.Master 1" 2>&1 | sed "s/^/$SPARK_MASTER_IP: /" &
+      oarsh $SPARK_MASTER_IP "SPARK_PREFIX=$SPARK_PREFIX ${SPARK_HOME}/sbin/spark-daemon-on-hpc.sh --config $SPARK_CONF_DIR stop org.apache.spark.deploy.master.Master 1" 2>&1 | sed "s/^/$SPARK_MASTER_IP: /" &
 
       # overwrite the 2 master ports in the local config (not sure why...)
       sed -i -e "s/^SPARK_MASTER_PORT=.*$/SPARK_MASTER_PORT=RANDOM/" -e "s/^SPARK_MASTER_WEBUI_PORT=.*$/SPARK_MASTER_WEBUI_PORT=RANDOM/" $SPARK_ENV
